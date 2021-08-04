812bf81d5dea15ebccf9ba9b51fd1a808fc555e8
andmalt <andrea.maltempi@gmail.com>
2021-08-04 15:33:31 +0200

    modificato js

--- Js/myscrypt.js
+++ Js/myscrypt.js
@@ -2,6 +2,6 @@ var Name = prompt("Qual'è il tuo nome?");
 var Surname = prompt("Qual'è il tuo cognome?");
 var favoriteColor = prompt("Qual'è il tuo colore preferito?")
 
-var passGen = Name + Surname + favoriteColor;
+var passGen = Name + Surname + favoriteColor + 21;
 
-document.getElementById("pass-gen").innerHTML = passGen+21; 
+document.getElementById("pass-gen").innerHTML = passGen; 
